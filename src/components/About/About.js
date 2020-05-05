1a5da15b64544d7a8ccf41d3e511e5597635a328
Daniel Hogan <hogan.daniel@hotmail.com>
2020-05-05 16:49:59 -0700

    css

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,18 +13,18 @@ const About = () => {
       </p>
       <p className="two">
         My name is <span>Danny Hogan</span>, I'm a software developer focused on
-        UX/UI and Front-End development. I love building applications with
-        modern web technologies such as React, Node.js, Express, MongoDB,
-        Socket.IO, and more.
+        UX/UI and Front-End development. I love building applications
+        with modern web technologies such as React, Node.js, Express, MongoDB,
+        Socket.IO, etc.
       </p>
       <p className="three">
         I'm currently seeking my next opportunity as a Front-End software
-        developer (open to remote opportunities)!
+        developer. Open to remote opportunities and relocation!
       </p>
       <div className="links">
         <p className="four">
           <a href="">
-            View Resume PDF
+            View Resume
             <MdPictureAsPdf />
           </a>
         </p>
