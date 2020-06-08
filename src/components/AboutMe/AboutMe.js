3c0ef26695076d7a37148875067f3b2f915a1c79
Daniel Hogan <hogan.daniel@hotmail.com>
2020-06-08 16:04:30 -0700

    about me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -11,22 +11,15 @@ const AboutMe = () => {
           <p>Danny Hogan - Full Stack Software Developer</p>
         </div>
         <p className="description-one">
-          Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah
-          Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah
-          Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah
-          Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah
-
+          As long as I can remember I've been fascinated by technology,
+          computers, and the internet. It has been a natural evolution to take
+          my passion for tech and turn it into a rewarding career.
         </p>
-        <ul>
-          <li>List Item 1</li>
-          <li>List Item 2</li>
-          <li>List Item 3</li>
-          <li>List Item 4</li>
-          <li>List Item 5</li>
-        </ul>
         <p className="description-one">
-          Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah
-          Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah Blah
+          I pride myself on being a tenacious full-stack developer, with a
+          designers eye and love for writing clean, maintainable, and elegant
+          code. I love to work collaboratively on teams - Let's build something
+          great!
         </p>
       </div>
     </section>
